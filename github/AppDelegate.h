22b2d9f094e2541391efea5ed56b5a842b05a9ca
DC20160426 <dc@zhangpengpeng.lan>
2016-09-18 09:31:34 +0800

    Initial Commit

---
+++ github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by DC20160426 on 16/9/18.
+//  Copyright © 2016年 DC20160426. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by DC20160426 on 16/9/18.
+//  Copyright © 2016年 DC20160426. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
